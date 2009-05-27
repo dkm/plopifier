85c7f5994811f8bf1cd6c22abfc0868ff5d02f8c
Marc Poulhiès <marc@g-miette.(none)>
2009-05-27 17:58:22 +0200

    updated test script

--- test.py
+++ test.py
@@ -3,7 +3,14 @@
 from plopifier import vimeo
 import sys
 
+
 # $1 : api key
 # $2 : api sig (secret)
 v = vimeo.Vimeo(sys.argv[1], sys.argv[2])
 v.get_frob_url()
+print v.get_auth_url(perms="write")
+print "waiting..."
+sys.stdin.read(1)
+v.get_auth_token()
+
+
