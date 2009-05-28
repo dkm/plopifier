d992409556089f36e80d452b3e62b3b70807e52b
Marc Poulhiès <marc@g-miette.(none)>
2009-05-28 13:45:25 +0200

    test script to set privacy for already uploaded vids

--- test.py
+++ test.py
@@ -12,7 +12,22 @@ v = vimeo.Vimeo(sys.argv[1], sys.argv[2], sys.argv[3])
 v.test_login()
 #v.upload("test.avi")
 
-v.set_title("4557213", "Paraglinding near Chamrousse (Belledone, France)")
-v.set_tags("4557213", ["paragliding", "parapente", 'chamrousse', 'toto'])
+l=[
 
+"4880163",
+"4880128",
+"4880103",
+"4880080",
+"4880058",
+"4880033",
+"4880004",
+"4879985",
+"4879962",
+"4879925",
+"4879925",
+"4879902",
+"4879879"]
 
+for i in l:
+    print "video:", i
+    v.set_privacy(i)
