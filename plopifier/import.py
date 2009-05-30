d1e5e320eaa35a19e268e825367c1a33cefc614c
Marc Poulhiès <marc@g-miette.(none)>
2009-05-30 09:07:48 +0200

    mainly cosemtic changes.  - changed method name to stick to vimeo's names (case and 'package')  - used a bit of pylint to catch main problems  - started to insert empty skeleton for missing API's method

--- plopifier/import.py
+++ plopifier/import.py
@@ -17,6 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Plopifier.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+This module helps to fill a database (currently sqlite based)
+with the images contained in a directories.
+The directories MUST be ordered by YEAR/MONTH/DAY
+"""
 
 from pysqlite2 import dbapi2 as sqlite
 import re
@@ -24,60 +29,53 @@ import sys
 import os
 from datetime import datetime
 
-file_pattern = re.compile("video@(?P<year>\d\d\d\d)(?P<month>\d\d)(?P<day>\d\d)(?P<hour>\d\d)(?P<min>\d\d)(?P<sec>\d\d)\.jpg")
+FILE_PATTERN = re.compile("video@(?P<year>\d\d\d\d)(?P<month>\d\d)" +
+                          "(?P<day>\d\d)(?P<hour>\d\d)(?P<min>\d\d)" + 
+                          "(?P<sec>\d\d)\.jpg")
 
+SQFILE = sys.argv[1]
+ROOT = sys.argv[2]
 
-sqfile = sys.argv[1]
-root = sys.argv[2]
+CONN = sqlite.connect(SQFILE)
+CUR = CONN.cursor()
+CONN.row_factory = sqlite.Row
 
-conn = sqlite.connect(sqfile)
-cur = conn.cursor()
-conn.row_factory = sqlite.Row
+COUNT = 0
 
-count = 0
+CURDIR = ROOT
 
-curdir = root
+q = "INSERT into repository (rootfs) VALUES('%s')" % ROOT
+CUR.execute(q)
+CONN.commit()        
 
-q = "INSERT into repository (rootfs) VALUES('%s')" %root
-cur.execute(q)
-conn.commit()        
+q = "SELECT id FROM repository WHERE rootfs = '%s'" % ROOT
+CUR.execute(q)
+ROOT_IDX = int(CUR.fetchall()[0][0])
+print "root_idx: %d" % ROOT_IDX
 
-q = "SELECT id FROM repository WHERE rootfs = '%s'" %root
-cur.execute(q)
-root_idx = int(cur.fetchall()[0][0])
-print "root_idx: %d" %root_idx
+INSERT_STR = "INSERT INTO images (relpath,ddate,rootid) VALUES ('%s', '%s', %s)"
 
-insert_str="INSERT INTO images (relpath,ddate,rootid) VALUES ('%s', '%s', %s)"
-
-def add_image(year,month,day,file):
-    pass
-
-
-
-for dirpath1,year_dirs,files1 in os.walk(curdir):
+for dirpath1, year_dirs, files1 in os.walk(CURDIR):
     for year in year_dirs:
-        for dirpath2,month_dirs,files2 in os.walk(os.path.join(dirpath1,year)):
+        for dirpath2, month_dirs, files2 in os.walk(os.path.join(dirpath1, year)):
             for month in month_dirs:
-                for dirpath3,day_dirs,files3 in os.walk(os.path.join(dirpath2,month)):
+                for dirpath3, day_dirs, files3 in os.walk(os.path.join(dirpath2, month)):
                     for day in day_dirs:
-                        for dirpath4,dirs4,image_files in os.walk(os.path.join(dirpath3,day)):
-                            for file in image_files:
-                                m = file_pattern.match(file)
+                        for dirpath4, dirs4, image_files in os.walk(os.path.join(dirpath3, day)):
+                            for filen in image_files:
+                                m = FILE_PATTERN.match(filen)
                                 if m == None:
                                     continue
-                                y,mt,d = int(year), int(month), int(day)
-                                h,m,s = int(m.group('hour')),int(m.group('min')),int(m.group('sec'))
+                                y, mt, d = int(year), int(month), int(day)
+                                h, m, s = int(m.group('hour')), int(m.group('min')), int(m.group('sec'))
                                 dt = datetime(y, mt, d, h, m, s)
-                                q = insert_str %(os.path.join(year,month,day,file),
-                                                 str(dt),
-                                                 str(root_idx))
-                                count+=1
-                                cur.execute(q)
-                                if count%100==0:
-                                    print "Count: %.10d\r" %count,
+                                q = INSERT_STR % (os.path.join(year, month, day, filen),
+                                                  str(dt),
+                                                  str(ROOT_IDX))
+                                COUNT += 1
+                                CUR.execute(q)
+                                if (COUNT % 100) == 0:
+                                    print "Count: %.10d\r" % COUNT,
 
-
-
-
-conn.commit()
-conn.close()
+CONN.commit()
+CONN.close()
